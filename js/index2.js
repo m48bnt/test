b316cf08872aa78ffea8c68a5256afeb09919783
Алексей В. Гурин <alex-vg@brit.co.ru>
2016-11-23 15:56:56 +0300

    2 versions

--- js/index2.js
+++ js/index2.js
@@ -23,15 +23,24 @@
         $('.updates').html(template(data));
       }
 
+      $(".stat-table th.table-command").data({ sortable: "true", "sortOrder": "desc", "key": "name" });
+      $(".stat-table th.table-matches").data({ sortable: "true", "sortOrder": "desc", "key": "matches" });
+      $(".stat-table th.table-wins").data({ sortable: "true", "sortOrder": "desc", "key": "win" });
+      $(".stat-table th.table-draws").data({ sortable: "true", "sortOrder": "desc", "key": "draw" });
+      $(".stat-table th.table-losses").data({ sortable: "true", "sortOrder": "desc", "key": "lose" });
+      $(".stat-table th.table-goals").data({ sortable: "true", "sortOrder": "desc", "key": "goals" });
+      $(".stat-table th.table-missing").data({ sortable: "true", "sortOrder": "desc", "key": "conceded_goals" });
+      $(".stat-table th.table-pts").data({ sortable: "true", "sortOrder": "desc", "key": "score" });
+
       // Событие сортировки по колонкам таблицы
       $(document).on("click", ".stat-table th", function (event) {
         var data = json;
-        var elem = event.currentTarget; // текущий показатель
-        var key = elem.getAttribute("data-key"); // ключ, по которому сортируется массив команд
-        var sortOrder = elem.getAttribute("data-sortorder"); // порядок сортировки (по убыванию / по возрастанию)
+        var elem = $(event.currentTarget); // текущий показатель
+        var key = elem.data("key"); // ключ, по которому сортируется массив команд
+        var sortOrder = elem.data("sortOrder"); // порядок сортировки (по убыванию / по возрастанию)
 
         // Проверка сортируется ли поле
-        if (elem.getAttribute("data-sortable")) {
+        if (elem.data("sortable")) {
           data.teams = json.teams.sort(function (team1, team2) {
             if (key === 'name') {
               var name1 = team1[key].toUpperCase();
@@ -46,7 +55,7 @@
           updateTable(data);
 
           // Меняем атрибут порядка сортировки
-          (sortOrder === 'desc') ? elem.setAttribute("data-sortorder", "asc") : elem.setAttribute("data-sortorder", "desc");
+          (sortOrder === 'desc') ? elem.data("sortOrder", "asc") : elem.data("sortOrder", "desc");
         }
       });
 
